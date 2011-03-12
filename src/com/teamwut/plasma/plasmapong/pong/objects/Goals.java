dac2c5838994b951322565db817f7d2a6a412427
rj <rj@rjs-macbook.local>
2011-03-12 03:35:30 -0600

    Awesome how portrait and landscape both work.

--- src/com/teamwut/plasma/plasmapong/pong/objects/Goals.java
+++ src/com/teamwut/plasma/plasmapong/pong/objects/Goals.java
@@ -23,9 +23,6 @@ public class Goals {
 	Bitmap bottom;
 	Bitmap middle;
 	Paint paint = new Paint();
-	Matrix topmat = new Matrix();
-	Matrix midmat = new Matrix();
-	Matrix botmat = new Matrix();
 	
 	public Goals(final PActivity p) {
 		this.p = p;
@@ -39,45 +36,17 @@ public class Goals {
 		middle = Drawbl.getGoalMiddle(p);
 		
 
-		topmat.setRotate(90, width/2, height/2);
-		midmat.setRotate(90, width/2, height/2);
-		botmat.setRotate(90, width/2, height/2);	
-
-		midmat.postTranslate(0, width/2-middle.getHeight()/2);
-		botmat.postTranslate(height-bottom.getWidth(), width-bottom.getHeight());
-
-		topmat.postTranslate(-(height-width)/2, (width-height)/2+width);
-//		midmat.postTranslate(-(height-width)/2, (width-height)/2);
-//		botmat.postTranslate(-(height-width)/2, (width-height)/2);
-
-
 	}
 
 	public void draw(final Canvas c) {
-		// for now
-//		p.stroke(goalColor);
-//		p.strokeWeight(3);
-//		p.line(goalBoarder, goalBoarder, goalBoarder, height - goalBoarder);
-//		p.line(width - goalBoarder, goalBoarder, width - goalBoarder, height
-//				- goalBoarder);
-//		p.imageMode(PApplet.CORNER);
-//		p.image(top, 0, 0);
-//		p.image(bottom, p.width-bottom.width, p.height-bottom.height);
-//		p.imageMode(PApplet.CENTER);
-//		p.image(middle, width/2, height/2);
-//		p.imageMode(PApplet.CORNER);
-		
 		if (Const.IS_PORTRAIT) {
 			c.drawBitmap(bottom, width-bottom.getWidth(), height-bottom.getHeight(), paint);
 			c.drawBitmap(middle, 0, height/2-middle.getHeight()/2, paint);
 			c.drawBitmap(top, 0, 0, paint);
 		} else {
-//			c.drawBitmap(bottom, botmat, paint);
-//			c.drawBitmap(middle, midmat, paint);
-//			c.drawBitmap(top, topmat, paint);
-			c.drawBitmap(bottom, width-bottom.getWidth(), height-bottom.getHeight(), paint);
+			c.drawBitmap(top, width-bottom.getWidth(), height-bottom.getHeight(), paint);
 			c.drawBitmap(middle, width/2-middle.getWidth()/2, 0,  paint);
-			c.drawBitmap(top, 0, 0, paint);
+			c.drawBitmap(bottom, 0, 0, paint);
 
 		}
 		
